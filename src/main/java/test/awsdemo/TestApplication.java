4ff725f635666da73b70c62c294941b5bebaa3b6
Kousik Mandal <kousik348@gmail.com>
2020-08-29 11:48:25 +0530

    test

---
+++ src/main/java/test/awsdemo/TestApplication.java
@@ -0,0 +1,21 @@
+package test.awsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestApplication.class, args);
+	}
+	
+	@RequestMapping("/test")
+	public String test() {
+		return "Working";
+	}
+
+}
